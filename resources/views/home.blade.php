3d4a7eb8a889765579051402896e9191e828bc7d
fabioferreiraf-git <fabio.ferreira@fingerdigital.com.br>
2021-10-10 18:23:47 -0300

    ajustes na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,8 +21,8 @@
     </nav>
 
     @if(session('get_ok'))
-    <div class="alert alert-success alert-dismissible" role="alert">        
-            <i class="fa fa-check-circle"></i>        
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <i class="fa fa-check-circle"></i>
         <span class="tex-success">{{session('get_ok')}}</span>
     </div>
     @endif
@@ -41,24 +41,72 @@
     </div>
     @endif
 
-    <div class="container my-5 bg-light">
-        <div class="row justify-content-center">
-            <div class="col-12">
+    <div class="container my-5 bg-light pt-5 pb-5">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
                 <div class="form-group ">
                     <form action="/home/submit" method="POST">
                         @csrf
                         <div class="row">
-                            <div class="col-2 offset-md-3">
-                                <label>Nome do carro:</label>
+                            <div class="col-5">
+                                <label class="text-primary">Nome do carro:</label>
                             </div>
                             <div class="col-5">
                                 <input class="form-control" id="termo" name="termo">
                             </div>
                             <div class="col-2">
-                                <input type="submit" class="btn btn-primary"></input>
+                                <button type="submit" class="btn btn-primary">Buscar</button>
                             </div>
                         </div>
                     </form>
+                </div>
+            </div>
+        </div>
+        <div class="row py-3">
+            <div class="col-12 d-flex justify-content-center ">
+                <div class="row">
+                    <div class="col-2 ">
+                        <form action="/home/submit" method="POST">
+                            @csrf
+                            <input type="hidden" class="form-control" id="termo" value="&carroceria=suv" name="termo">
+                            <button type="submit" class="btn btn-warning">SUV</button>
+                        </form>
+                    </div>
+                    <div class="col-2">
+                        <form action="/home/submit" method="POST">
+                            @csrf
+                            <input type="hidden" class="form-control" id="termo" value="&carroceria=hatch" name="termo">
+                            <button type="submit" class="btn btn-warning">HATCH</button>
+                        </form>
+                    </div>
+                    <div class="col-2">
+                        <form action="/home/submit" method="POST">
+                            @csrf
+                            <input type="hidden" class="form-control" id="termo" value="&carroceria=picape" name="termo">
+                            <button type="submit" class="btn btn-warning">PICK-UP</button>
+                        </form>
+                    </div>
+                    <div class="col-2">
+                        <form action="/home/submit" method="POST">
+                            @csrf
+                            <input type="hidden" class="form-control" id="termo" value="&carroceria=seda" name="termo">
+                            <button type="submit" class="btn btn-warning">SEDAN</button>
+                        </form>
+                    </div>
+                    <div class="col-2">
+                        <form action="/home/submit" method="POST">
+                            @csrf
+                            <input type="hidden" class="form-control" id="termo" value="&carroceria=perua" name="termo">
+                            <button type="submit" class="btn btn-warning">PERUA</button>
+                        </form>
+                    </div>
+                    <div class="col-2">
+                        <form action="/home/submit" method="POST">
+                            @csrf
+                            <input type="hidden" class="form-control" id="termo" value="&carroceria=minivan" name="termo">
+                            <button type="submit" class="btn btn-warning">MINIVAN</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
